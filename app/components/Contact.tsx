3d97fc4406aa0bc89182dd0bd6123516dee938b8
Jaydmart <107052448+Jaydmart@users.noreply.github.com>
2025-01-30 13:32:51 -0500

    Update Contact.tsx

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <section>
-      <h2 className="text-3x1 font-bold mb-8 dark:text-white">Let's Work Together</h2>
+      <h2 className="text-3xl font-bold mb-8 dark:text-white">Let's Work Together</h2>
       <p className="mb-4 dark:text-gray-300">
         I'm always open to new opportunities and collaborations. If you're interested in working together, feel free to reach out!
       </p>
