8325c22131d11f86bf8eef79b3d2cca5fc2739cc
Jaydmart <107052448+Jaydmart@users.noreply.github.com>
2025-02-17 10:06:03 -0500

    Update menubar.tsx

--- components/ui/menubar.tsx
+++ components/ui/menubar.tsx
@@ -11,3 +11,16 @@ const MenubarMenu = MenubarPrimitive.Menu
 const MenubarGroup = MenubarPrimitive.Group
 
 const MenubarPortal = MenubarPrimitive.Portal
+
+const MenubarSub = MenubarPrimitive.Sub
+
+const MenubarRadioGroup = MenubarPrimitive.RadioGroup
+
+const Menubar = React.forwardRef<
+  React.ElementRef<typeof MenubarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Root>
+>(({ className, ...props }, ref, => (
+  <MenubarPrimitive.Root
+    ref={ref}
+    className={cn(
+      "flex h-10 items-center space-x-1 rounded-md border bg-background
