91c9fd9f059a40c6ea354f2306cd8beb454b0668
Jaydmart <107052448+Jaydmart@users.noreply.github.com>
2025-02-20 20:37:20 -0500

    Update menubar.tsx

--- components/ui/menubar.tsx
+++ components/ui/menubar.tsx
@@ -72,3 +72,6 @@ const MenubarSubContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof MenubarPrimitive.SubContent>
 >(({ className, ...props }, ref) => (
   <MenubarPrimitive.SubContent
+    ref={ref}
+    className={cn(
+      "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 
